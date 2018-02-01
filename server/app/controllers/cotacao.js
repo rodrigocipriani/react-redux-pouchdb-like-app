8ed6c435f00bdb0de9b41802fcebcecec2ded2ff
Rodrigo Cipriani da Rosa <chopsss@gmail.com>
2018-01-31 22:14:20 -0200

    Criado route e service com cotações CoinBr

--- server/app/controllers/cotacao.js
+++ server/app/controllers/cotacao.js
@@ -1,63 +1,63 @@
 // const BrError   = require('bb-common/util/br-error');
 // const BrRequest = require('bb-common/util/br-request');
-const config = require('../../config/config');
+// const config = require('../../config/config');
 
 const BrError = require('../helpers/error_constructor');
 
 module.exports = (app) => {
-  
   const BitcoinTradeService   = app.services.bitcoin_trade;
   const CoinExService         = app.services.coin_ex;
   const MercadoBitcoinService = app.services.mercado_bitcoin;
   const CoinBrService = app.services.coin_br;
-  
-  const sequelize  = app.models.models.sequelize;
+
+  // const sequelize  = app.models.models.sequelize;
   const controller = {};
-  
+
+  /**
+   * Busca cotações para Coin BR
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
   controller.consultarCotacoesCoingBr = async (req, res, next) => {
     const cotacoes = await CoinBrService.consultarCotacoesCoingBr();
     console.log('cotacoes Coin Br', cotacoes);
-    if(!cotacoes){ return next(new BrError(err)); }
+    if (!cotacoes) { return next(new BrError('Falha ao buscar cotações para Coin Br')); }
     res.json(cotacoes);
   };
 
-  controller.calcularCompra                = (req, res, next) => {
-    
-    console.log("req", req.query);
-    const {valor, moeda} = req.query;
-    
+  controller.calcularCompra = (req, res, next) => {
+    console.log('req', req.query);
+    const { valor, moeda } = req.query;
+
     return BitcoinTradeService.recuperarQuantidadeMoeda(valor, moeda)
     .then((quantidade) => {
-      console.log("quantidade", cotacoes.data);
+      console.log('quantidade', cotacoes.data);
       res.status(200).send(cotacoes.data);
     }).catch(err => {
-      console.log("aqui", err);
-      
+      console.log('aqui', err);
+
       return next(new BrError(err));
     });
-    
   };
-  controller.consultarOrdensBtcTrade       = (req, res, next) => {
-    
+  controller.consultarOrdensBtcTrade = (req, res, next) => {
     return BitcoinTradeService.recuperarOfertas()
     .then((cotacoes) => {
-      console.log("cotacoes", cotacoes.data);
+      console.log('cotacoes', cotacoes.data);
       res.status(200).send(cotacoes.data);
     }).catch(err => {
-      console.log("aqui", err);
-      
+      console.log('aqui', err);
+
       return next(new BrError(err));
     });
-    
   };
   controller.consultarOrdensMercadoBitcoin = (req, res, next) => {
-    
-    const {valor, moeda} = req.query;
+    const { valor, moeda } = req.query;
     const moedasVendidas = ['BTC', 'BCH', 'LTC'];
     const comissao       = 0.007;
     const taxa           = 0.0001582;
-    let cotacoes         = [];
-    
+    const cotacoes         = [];
+
     for (const moedaAtual of moedasVendidas) {
       cotacoes.push(
         MercadoBitcoinService.recuperarOfertas(moedaAtual)
@@ -66,30 +66,26 @@ module.exports = (app) => {
         .then(quantidade => calcularMoedas(quantidade, moedaAtual, moeda))
       );
     }
-    
-    console.log("ponto 2");
+
+    console.log('ponto 2');
     const resultados = Promise.all(cotacoes);
-    
+
     resultados.then(data => {
-      console.log("MEU", data);
+      console.log('MEU', data);
       res.status(200).send(cotacoes);
     }).catch(err => {
-      console.log("aqui", err);
+      console.log('aqui', err);
       return next(new BrError(err));
     });
-    
-    
   };
-  
+
   const calcularQuantidadeValor = (ofertas, valor, comissao) => {
-    
     let valorRestante   = valor;
     let quantidadeTotal = 0;
-    
-    for (let oferta of ofertas) {
-      
+
+    for (const oferta of ofertas) {
       const quantidadePossivel = valorRestante / oferta[0];
-      
+
       if (quantidadePossivel <= oferta[1]) {
         quantidadeTotal += quantidadePossivel;
         break;
@@ -97,47 +93,40 @@ module.exports = (app) => {
         valorRestante -= oferta[0] * oferta[1];
         quantidadeTotal += oferta[1];
       }
-      
     }
-    
+
     quantidadeTotal *= (1 - comissao);
-    
+
     return quantidadeTotal;
   };
   const calcularMoedas          = (quantidade, moedaAtual, moedaNova) => {
-    
     if (moedaAtual === moedaNova) return quantidade;
-    
+
     switch (moedaAtual) {
-      case 'BCH':
-        return CoinExService.recuperarOfertas(moedaNova, 10)
+    case 'BCH':
+      return CoinExService.recuperarOfertas(moedaNova, 10)
         .then(cotacoes => converterMoeda(cotacoes.bids, quantidade))
         .then(novaQuantidade => calcularMoedas(novaQuantidade, moedaNova, moedaNova));
-      default:
-        return CoinExService.recuperarOfertas(moedaNova, 10)
+    default:
+      return CoinExService.recuperarOfertas(moedaNova, 10)
         .then(cotacoes => converterMoeda(cotacoes.asks, quantidade))
         .then(novaQuantidade => calcularMoedas(novaQuantidade, 'BCH', moedaNova));
     }
-    
   };
   const calcularTransferencia   = (quantidade, taxa, moedaAtual, moedaNova) => {
-    
     if (moedaAtual === moedaNova) return quantidade;
-    
+
     return quantidade - taxa;
-    
   };
   const converterMoeda          = (ofertas, quantidadeOrigem) => {
-    
     let quantidadeRestante = quantidadeOrigem;
     let quantidadeDestino  = 0;
-    
+
     // console.log("q", ofertas);
-    
-    for (let oferta of ofertas) {
-      
+
+    for (const oferta of ofertas) {
       const quantidadePossivel = quantidadeRestante / Number(oferta[0]);
-      
+
       if (quantidadePossivel <= oferta[1]) {
         quantidadeDestino += quantidadePossivel;
         break;
@@ -145,18 +134,11 @@ module.exports = (app) => {
         quantidadeRestante -= Number(oferta[0]) * Number(oferta[1]);
         quantidadeDestino += Number(oferta[1]);
       }
-      
     }
-    
+
     return quantidadeDestino;
   };
-  
+
   return controller;
-  
 };
-
-
-
-
-
 
--- server/app/routes/cotacao.js
+++ server/app/routes/cotacao.js
@@ -7,15 +7,13 @@ const config = require('../../config/config');
 
 module.exports = app => {
   const controller = app.controllers.cotacao;
-  
+
   // app.route(`${config.apiUrl}/v1/deslocamento/listar/proprias/vigentes`)
   //   // .get( controller.consultarIndicador);
   //   .get(acesso.validarAcesso(3, { token: config.accessToken }), controller.consultarIndicador);
-  
+
   app.route(`${ config.apiUrl }/v1/cotacao/coin-br/consultar`).get(controller.consultarCotacoesCoingBr);
   app.route(`${ config.apiUrl }/v1/cotacao/bitcoin-trade/consultar`).get(controller.consultarOrdensBtcTrade);
   app.route(`${ config.apiUrl }/v1/cotacao/mercado-bitcoin/consultar`).get(controller.consultarOrdensMercadoBitcoin);
   app.route(`${ config.apiUrl }/v1/compra/moeda/consultar`).get(controller.calcularCompra);
-  
-
 };
